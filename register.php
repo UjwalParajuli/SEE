b0d977dccd1570e2cfb4529cca29b98cc35470c2
Ujwal Parajuli <uzwalparajuli07@gmail.com>
2020-02-09 20:48:28 +0545

    Add files via upload

---
+++ register.php
@@ -0,0 +1,40 @@
+<?php
+$servername = "localhost";
+$dbname = "id5461621_see";
+$uname = "id5461621_event";
+$pass = "12345";
+$conn = mysqli_connect($servername, $uname, $pass, $dbname);
+if (!$conn) {
+	die("Connection failed!");
+}
+
+$code = (int)$_POST['code'];
+$full_name = $_POST['full_name'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+$phone = $_POST['phone'];
+$address = $_POST['address'];
+
+$sql = "select * from verification where email = '$email' and code = $code";
+$result = mysqli_query($conn, $sql);
+if (mysqli_num_rows($result) < 1) {
+	echo "error";
+		
+}
+else {
+	$hashed_password = password_hash($password, PASSWORD_DEFAULT);
+	$qry = "insert into user (full_name, email, password, phone, address) values ('$full_name' , '$email', '$hashed_password', '$phone', '$address')";
+			
+	$result = mysqli_query($conn, $qry);
+	if(!$result){
+		echo "dbError";
+	}
+	else{
+		echo "success";
+	}
+}
+	
+?>
+
+
+
---
+++ verify.php
@@ -0,0 +1,41 @@
+<?php
+$servername = "localhost";
+$dbname = "id5461621_see";
+$uname = "id5461621_event";
+$pass = "12345";
+$conn = mysqli_connect($servername, $uname, $pass, $dbname);
+if (!$conn) {
+	die("Connection failed!");
+}
+
+$email_address = strip_tags(htmlspecialchars($_POST['email']));
+$random = (int)rand(1000,50000);
+
+$sql = "select * from user where (email = '$email_address');";
+$result = mysqli_query($conn, $sql);
+if (mysqli_num_rows($result) > 0) {
+	$row = mysqli_fetch_assoc($result);
+
+	if ($email_address == $row['email']) {
+		echo "error";
+	}
+		# code...
+}
+else {
+	$qry = "insert into verification (email, code) values ('$email_address' , $random)";
+			
+	$result = mysqli_query($conn, $qry);
+	if(!$result){
+		echo "dbError";
+	}
+	else{
+		$to = $email_address; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+		$email_subject = "Email Verification Code";
+		$email_body = "Please enter this code to verify your account. Thank You for signing up.\n\n".$random;
+		$headers = "From: info@ujwalparajuli.com.np\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+		$headers .= "Reply-To: $email_address";   
+		mail($to,$email_subject,$email_body,$headers);
+		echo "success";
+	}
+}
+?>
